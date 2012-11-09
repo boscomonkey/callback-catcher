728fdea0b40948a09a31001444b307bc4dd67f7a
Bosco So <bosco@indiegogo.com>
2012-11-08 20:10:16 -0800

    Initial Rails 3.2.8 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CallbackCatcher::Application.config.secret_token = '0e0cf5a2c6e4e465eddf973e216e630d1d4ae26c1131e7e4a26078f178e2741e46d93520178a5c2b49a9fb34ca4b5a683b54a3076690b50809f2751db28ceaa0'
