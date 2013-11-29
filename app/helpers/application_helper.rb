c77cafe96e5db870ef64af7269177ec8e08a46fc
Bosco So <bosco@indiegogo.com>
2013-11-28 23:38:24 -0800

    displaying new "headers" field in edit/show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def line_length(str)
+    lines = str.split(/\n/)
+    lines.push('')		# insure at least one zero length line
+
+    sizes = lines.map(&:size)
+    sizes.max
+  end
+
+  def num_lines(str)
+    str.split(/\n/).size
+  end
 end
