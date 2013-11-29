49a18c7bca990dd0aa0cd16d2842003be2edb601
Bosco So <bosco@indiegogo.com>
2013-11-29 00:32:28 -0800

    now handles requests without cookies

--- lib/filter_cookie_values.rb
+++ lib/filter_cookie_values.rb
@@ -3,8 +3,9 @@
 # as
 # "_callback-catcher_session=[FILTERED];first=[FILTERED];last=[FILTERED]"
 def filter_cookie_values(headers, key, replacement:'[FILTERED]', separator:';')
-  cookies = headers[key]
-  lines = cookies.split(separator)
-  filtered = lines.map {|line| line.sub /\=.*/, "=#{replacement}" }
-  headers[key] = filtered.join(separator)
+  if cookies = headers[key]
+    lines = cookies.split(separator)
+    filtered = lines.map {|line| line.sub /\=.*/, "=#{replacement}" }
+    headers[key] = filtered.join(separator)
+  end
 end
